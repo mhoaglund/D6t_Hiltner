b205bdc53ca60e3f86a9c583c8f69eb92d74d60e
mhoaglund <maxwell.hoaglund@gmail.com>
2021-03-22 20:47:48 -0500

    patching in old algae threat matrix code

--- OutputCaboose/OutputCaboose.ino
+++ OutputCaboose/OutputCaboose.ino
@@ -1,7 +1,7 @@
 //Installed on the last uno in the chain, it just listens and complies.
 
-const long interval = 25;
-const long alt_interval = 500;
+int interval = 125;
+int alt_interval = 500;
 long previousMillis = 0;
 long previousMillisAlt = 0;
 boolean newData = false;
@@ -9,7 +9,19 @@ boolean shouldRun = false;
 String cmdcache = "";
 byte VERSION = 1;
 
-int GROUP1[] = {8, 9, 10, 11};
+int DEVICES[] = {8, 9, 10, 11};
+boolean DIR = true;
+int DEVICE_COUNT = 4;
+const byte MAX_TICKS = 25; //number of ticks in a measure of time
+byte TICKS = 1;
+const byte STATECHANGES[] = {1,7,14,19}; //device changeover points
+
+//At times, pump timing will be varied by small random amounts. Pauses will also be inserted.
+byte STATECHANGE_AUX_STARTS[] = {0,0,0,0};
+byte STATECHANGE_AUX_PAUSES[] = {0,0,0,0};
+byte STATECHANGE_AUX_RAND_PAUSES[] = {0,0,0,0};
+
+byte stochasticity = 4;
 
 const byte numChars = 32;
 char receivedChars[numChars];
@@ -24,9 +36,9 @@ void setup()
   Serial.begin(9600);
   Serial.println("lets get started");
  
-  for (int i = 0; i < 4; i = i + 1) {    
-     pinMode(GROUP1[i], OUTPUT);
-     digitalWrite(GROUP1[i], HIGH);
+  for (int i = 0; i < DEVICE_COUNT; i = i + 1) {    
+     pinMode(DEVICES[i], OUTPUT);
+     digitalWrite(DEVICES[i], HIGH);
    }
 
   Serial.begin(9600);
@@ -45,21 +57,55 @@ void loop()
   }
   if(currentMillis - previousMillis > interval) {
     previousMillis = currentMillis; 
-    Iterate();
+    if(shouldRun){
+      Iterate();
+    }
   }
   if(currentMillis - previousMillisAlt > alt_interval) {
     previousMillisAlt = currentMillis; 
-    UpdateActivationOrder();
+    if(shouldRun){
+      DecorateCycle();  
+    }
   }
 }
 
-void UpdateActivationOrder(){
-  //TODO repopulate run array
+void Iterate(){
+  if(TICKS >= MAX_TICKS){
+    //TODO stochastic redecorate? or will that always be on another loop?
+    TICKS = 1;
+  }
+  for(byte i = 0; i < DEVICE_COUNT; i++){
+    if(DIR){
+      if(TICKS < STATECHANGE_AUX_STARTS[i] | TICKS == STATECHANGE_AUX_PAUSES[i]){
+        digitalWrite(DEVICES[i], HIGH);
+      }
+      else digitalWrite(DEVICES[i], LOW);
+    }
+    else{
+      if(TICKS > STATECHANGES[i]){
+        digitalWrite(DEVICES[i], LOW);
+      }
+      else digitalWrite(DEVICES[i], HIGH);
+    }
+  }
+  TICKS++;
 }
 
-void Iterate(){
-  //TODO move to next frame of action
+void DecorateCycle(){
+  Serial.println("Decorating...");
+  for(byte i = 0; i < DEVICE_COUNT; i++){
+     int base = STATECHANGES[i];
+     byte _newpause = random(base, base+stochasticity);
+     byte _randpause = random(1, MAX_TICKS);
+     byte _newstart = random(base-stochasticity, base);
+     if(_newstart < 1) _newstart = 1;
+     if(_newpause > MAX_TICKS) _newpause = MAX_TICKS;
+     STATECHANGE_AUX_STARTS[i] = _newstart;
+     STATECHANGE_AUX_PAUSES[i] = _newpause;
+     STATECHANGE_AUX_RAND_PAUSES[i] = _randpause;
+  }
 }
+
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
     static byte ndx = 0;
@@ -103,12 +149,21 @@ void applySerialCommand(String serialcommand){
         //TODO adjust rate of cycling
         serialcommand.remove(0,1);
         int newrate = serialcommand.toInt();
+        if(newrate < 50) newrate = 50; //avoid burning out the relays
         Serial.println(newrate, DEC);
+        if(newrate == 0 && shouldRun){
+          Stop();
+        }
+        else if(!shouldRun && newrate > 0){
+          Start();
+        }
+        interval = newrate;
       }
       else if(serialcommand[0] == adjuststoch_flag){
         serialcommand.remove(0,1);
-        int stochasticity = serialcommand.toInt();
-        Serial.println(stochasticity, DEC);
+        int stoch = serialcommand.toInt();
+        Serial.println(stoch, DEC);
+        alt_interval = stoch;
       }
       else if(serialcommand[0] == stop_flag){
         Stop();
@@ -122,6 +177,9 @@ void Stop(){
   Serial.println("Stopping");
    if(shouldRun){
     shouldRun = false;
+   }
+   for (int i = 0; i < DEVICE_COUNT; i = i + 1) {    
+     digitalWrite(DEVICES[i], HIGH);
    }
 }
 
